1aae9df78b37676ea17e5854a7f40823d08eb0ec
Eyal Toledano <etoledano@playigl.com>
2013-12-20 13:50:38 -0500

    Initial commit of Localify landing site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localify::Application.config.secret_token = 'e4545fb9bfbc99096f1a647031f5f9ea69f1f0917845926d9fb8b348bd618eac4a34032c7a127d02cef0c8a4b64dd108be124c0e2fb3ffc09936e8db703a0b65'
